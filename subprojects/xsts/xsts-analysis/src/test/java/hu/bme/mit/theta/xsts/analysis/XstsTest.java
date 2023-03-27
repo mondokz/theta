42fa14d2be13b044e65ce1b860229da4b226d337
mondokz <90513897+mondokz@users.noreply.github.com>
2023-03-27 16:30:42 +0200

    com

--- subprojects/xsts/xsts-analysis/src/test/java/hu/bme/mit/theta/xsts/analysis/XstsTest.java
+++ subprojects/xsts/xsts-analysis/src/test/java/hu/bme/mit/theta/xsts/analysis/XstsTest.java
@@ -243,7 +243,7 @@ public class XstsTest {
 		}
 		Xsts_K_induction checker= new Xsts_K_induction();
 		//final XstsConfig<?, ?, ?> configuration = new XstsConfigBuilder(domain, XstsConfigBuilder.Refinement.SEQ_ITP, Z3SolverFactory.getInstance()).initPrec(XstsConfigBuilder.InitPrec.CTRL).optimizeStmts(XstsConfigBuilder.OptimizeStmts.ON).predSplit(XstsConfigBuilder.PredSplit.CONJUNCTS).maxEnum(250).autoExpl(XstsConfigBuilder.AutoExpl.NEWOPERANDS).logger(logger).build(xsts);
-		final SafetyResult<?, ?> status = checker.check(xsts,100,Z3SolverFactory.getInstance().createSolver());
+		final SafetyResult<?, ?> status = checker.check(xsts,50,Z3SolverFactory.getInstance().createSolver());
 		if (safe) {
 			assertTrue(status.isSafe());
 		} else {
