f6d0cca0e0c715f84e1c52d025ada1a87bc30a71
mondokz <90513897+mondokz@users.noreply.github.com>
2023-04-18 22:51:35 +0200

    dependency-fix

--- subprojects/common/analysis/build.gradle.kts
+++ subprojects/common/analysis/build.gradle.kts
@@ -6,8 +6,5 @@ dependencies {
     implementation(project(":theta-common"))
     implementation(project(":theta-core"))
     implementation(project(":theta-solver"))
-    implementation(project(mapOf("path" to ":theta-xsts-analysis")))
-    implementation(project(mapOf("path" to ":theta-xsts-analysis")))
-    implementation(project(mapOf("path" to ":theta-xsts-analysis")))
     testImplementation(project(":theta-solver-z3"))
 }
