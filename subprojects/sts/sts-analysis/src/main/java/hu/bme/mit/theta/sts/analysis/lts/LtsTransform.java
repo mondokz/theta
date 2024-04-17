d66c5dbd317e8c72d4129787e0aa8e8244ad3150
mondokz <90513897+mondokz@users.noreply.github.com>
2024-04-17 17:36:43 +0200

    lts tansform refactor

--- subprojects/sts/sts-analysis/src/main/java/hu/bme/mit/theta/sts/analysis/lts/LtsTransform.java
+++ subprojects/sts/sts-analysis/src/main/java/hu/bme/mit/theta/sts/analysis/lts/LtsTransform.java
@@ -1,21 +1,19 @@
 
 package hu.bme.mit.theta.sts.analysis.lts;
 
-import hu.bme.mit.theta.common.Tuple3;
+import hu.bme.mit.theta.analysis.algorithm.bounded.MonolithicExpr;
+
+import hu.bme.mit.theta.common.container.Containers;
 import hu.bme.mit.theta.core.decl.Decls;
 import hu.bme.mit.theta.core.decl.VarDecl;
 import hu.bme.mit.theta.core.stmt.*;
 import hu.bme.mit.theta.core.type.Expr;
 import hu.bme.mit.theta.core.type.Type;
-import hu.bme.mit.theta.core.type.abstracttype.EqExpr;
-import hu.bme.mit.theta.core.type.arraytype.ArrayEqExpr;
-import hu.bme.mit.theta.core.type.booltype.AndExpr;
-import hu.bme.mit.theta.core.type.booltype.BoolExprs;
 import hu.bme.mit.theta.core.type.booltype.BoolType;
-import hu.bme.mit.theta.core.type.booltype.SmartBoolExprs;
+import hu.bme.mit.theta.core.utils.ExprUtils;
 import hu.bme.mit.theta.core.utils.StmtUtils;
 import hu.bme.mit.theta.core.utils.indexings.VarIndexingFactory;
-import hu.bme.mit.theta.sts.STS;
+
 
 
 import java.util.*;
@@ -26,36 +24,56 @@ import static hu.bme.mit.theta.core.type.booltype.SmartBoolExprs.And;
 import static hu.bme.mit.theta.core.type.booltype.SmartBoolExprs.Not;
 
 public class LtsTransform {
-    public static Tuple3<Expr<BoolType>, Expr<BoolType>, Expr<BoolType>> lts(STS sts) {
+
+    MonolithicExpr monolithicExpr;
+    Collection<VarDecl<?>> vars;
+    VarDecl<BoolType> saved;
+
+    public LtsTransform(MonolithicExpr monolithicExpr){
+        this.monolithicExpr = monolithicExpr;
+        this.saved = Decls.Var("saved",BoolType.getInstance());
+
+        final Set<VarDecl<?>> tmpVars = Containers.createSet();
+        ExprUtils.collectVars(monolithicExpr.getInitExpr(), tmpVars);
+        ExprUtils.collectVars(monolithicExpr.getTransExpr(), tmpVars);
+        ExprUtils.collectVars(monolithicExpr.getPropExpr(), tmpVars);
+        this.vars = Collections.unmodifiableCollection(tmpVars);
+    }
+
+    public Expr<BoolType> getInitFunc(){
+        return And(monolithicExpr.getInitExpr(),Not(saved.getRef()));
+    }
+    public Expr<BoolType> getTransFunc(){
+
         ArrayList<Stmt> skip = new ArrayList<>(Collections.singleton(SkipStmt.getInstance()));
         var assignList = new ArrayList<Stmt>();
-        Expr<BoolType> prop = True();
 
-        var saved = Decls.Var("saved",BoolType.getInstance());
-
-        var init = And(sts.getInit(),Not(saved.getRef()));
-
-        for (var varDecl : sts.getVars()) {
+        for (var varDecl : vars) {
             var newVar = Decls.Var(varDecl.getName(), varDecl.getType());
-
             assignList.add(AssignStmt.of((VarDecl<Type>)newVar, (Expr<Type>) varDecl.getRef()));
-
-            var exp = Eq(newVar.getRef(),varDecl.getRef());
-            prop = And(prop,exp);
-
         }
-        prop = Not(And(prop,sts.getProp(),saved.getRef()));
 
         assignList.add(AssignStmt.of(saved, True()));
         var seq = SequenceStmt.of(assignList);
         skip.add(seq);
+
         var nonDet = NonDetStmt.of(skip);
         var saveOrSkip = StmtUtils.toExpr(nonDet, VarIndexingFactory.indexing(0)).getExprs();
-
-        var t = new ArrayList<>(Collections.singleton(sts.getTrans()));
+        var t = new ArrayList<>(Collections.singleton(monolithicExpr.getTransExpr()));
         t.addAll(saveOrSkip);
-        var tran = And(t);
 
-        return Tuple3.of(init, tran, prop);
+        return And(t);
+    }
+    public Expr<BoolType> getProp(){
+
+        Expr<BoolType> prop = True();
+
+        for (var varDecl : vars) {
+            var newVar = Decls.Var(varDecl.getName(), varDecl.getType());
+            var exp = Eq(newVar.getRef(),varDecl.getRef());
+            prop = And(prop,exp);
+        }
+
+        return Not(And(prop,monolithicExpr.getPropExpr(),saved.getRef()));
     }
 }
